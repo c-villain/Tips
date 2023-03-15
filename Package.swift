40bd73fe4ef19e520a3545fdae07be3e68bff975
Alexander Kraev <lexkraev@gmail.com>
2023-03-15 20:59:02 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "Cloudtips",
+            name: "Tips",
             targets: ["CloudtipsTarget"]),
     ],
     dependencies: [
