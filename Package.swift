24fd2497a1559cfe8244814bddd8effbc6d10731
Alexander Kraev <lexkraev@gmail.com>
2023-03-16 17:10:11 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,6 @@ let package = Package(
         .library(
             name: "CloudtipsSPM",
             targets: ["CloudtipsTarget"])
-        ,.library(name: "SnapKitFromTips",
-                 targets: ["SnapKit"])
     ],
     dependencies: [
     ],
